80fc062c01cdb6628eddffc45ca2ad97f3702b34
Zachary Durber <zdurber@gmail.com>
2015-02-22 22:22:33 +0800

    Retrieving data and displaying it. Table and map view.

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 <head>
-<?php 
+<?php
+	require_once('config.php');
 	$connection = mysqli_connect('localhost', 'root', '', 'zimtravel');
 	// Error on fail.
 	if (mysqli_connect_errno()) {
@@ -12,11 +13,34 @@
 		$travelevents = array();
 		if ($result) {
 			while ($travelevents[] = $result->fetch_assoc()) {
-				//
+				// Build array.
 			}
 		}
 	}
 	$editicon = "<img src='https://cdn2.iconfinder.com/data/icons/flat-ui-icons-24-px/24/new-24-128.png' style='height: 16; width: 16;'>";
+
+	// Init table.
+	$table = "<table><tr>";
+
+	// Table headers.
+	foreach (array_slice($travelevents[0], 1) as $key => $value) {
+		$table = $table . "<th>" . $key . "</th>";
+	}
+	$table = $table . "</tr>";
+
+	// Table content.
+	foreach ($travelevents as $key => $value) {
+		if ($value) {
+			$table = $table . "<tr>";	
+			foreach (array_slice($value, 1) as $key => $value) {
+				$table = $table . "<td>" . $value . "</td>";
+			}
+			$table = $table . "</tr>";
+		}
+	}
+
+	// Table end.
+	$table = $table . "</table>";
 
 ?>
 <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css" />
@@ -26,10 +50,12 @@
 <h1>ZIM2015</h1>
 <h2>South America adventure</h2>
 <div id="map" style="height: 500px; width: 500px;  position: relative;"></div>
-<div id="table"><table>
+<div id="table"><?php echo $table; ?></div>
+</body>
 <script>
 // create a map in the "map" div, set the view to a given place and zoom
 var map = L.map('map').setView([-33.4500, -70.6667], 8);
+refreshMap();
 
 // add an OpenStreetMap tile layer
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
@@ -37,35 +63,20 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Add event to map.
-var travelevents = <?php echo json_encode($travelevents); ?>;
-var latlngs = [];
-if (travelevents) {
-	console.log(travelevents[0]);
-	var headers = [];
-	document.write('<tr><th>action</th>');
-	for (var i = 1; i < Object.keys((travelevents)[0]).length; i++) {
-		headers[i] = Object.keys((travelevents)[0])[i];
-		document.write('<th>' + headers[i] + '</th>');
+function refreshMap() {
+	var travelevents = <?php echo json_encode($travelevents); ?>;
+	var latlngs = [];
+
+	if (travelevents) {
+		for (var i = 0; i < travelevents.length - 1; i++) {
+			latlngs.push(new L.LatLng(travelevents[i].lat, travelevents[i].long));
+			L.marker([travelevents[i].lat, travelevents[i].long]).addTo(map)
+		    .bindPopup(travelevents[i].title)
+		    .openPopup();
+		};
+
+		var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
 	}
-	document.write('</tr>');
-	for (var i = 0; i < travelevents.length - 1; i++) {
-		document.write('<tr>');
-		document.write("<td><?php echo $editicon; ?></td>"); // Empty for now.
-		for (var j = 1; j < Object.keys((travelevents)[0]).length; j++) {
-			document.write('<td>' + travelevents[i][headers[j]] + '</td>');
-		}
-		latlngs.push(new L.LatLng(travelevents[i].lat, travelevents[i].long));
-		L.marker([travelevents[i].lat, travelevents[i].long]).addTo(map)
-	    .bindPopup(travelevents[i].title)
-	    .openPopup();
-	    document.write('</tr>');
-	};
-	console.log(latlngs);
-	var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
-	
 }
-    </script>
-    </table>
-    </div>
-</body>
+</script>
 </html>
